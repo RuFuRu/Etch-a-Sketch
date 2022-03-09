30fb6b71e91b3e6fe39d2445b3bdad82f8efa8a8
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-03-09 18:48:29 +0100

    Revert "added the grid and centred it"
    This reverts commit 98afe681e03492c1d6e29d0b7e8b0ac23bec4858.

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-const container = document.querySelector('.container');
-let squareDiv;
-
-for(let i = 0; i < 256; i++) {
-    squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class', 'square-div');
-    container.appendChild(squareDiv);
-}
-
-
-
+const squareDiv = document.createElement('div');
+squareDiv.setAttribute('class', 'square-div');
