98afe681e03492c1d6e29d0b7e8b0ac23bec4858
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-03-09 18:41:59 +0100

    added the grid and centred it

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const squareDiv = document.createElement('div');
-squareDiv.setAttribute('class', 'square-div');
+const container = document.querySelector('.container');
+let squareDiv;
+
+for(let i = 0; i < 256; i++) {
+    squareDiv = document.createElement('div');
+    squareDiv.setAttribute('class', 'square-div');
+    container.appendChild(squareDiv);
+}
+
+
+
