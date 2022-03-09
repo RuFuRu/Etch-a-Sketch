d0d2ad098cfb09fd995428837b0977fc223eda74
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-03-09 19:48:13 +0100

    added ClickandHold class

--- script.js
+++ script.js
@@ -9,8 +9,40 @@ for(let i = 0; i < 256; i++) {
 
 const newSquareDiv = document.querySelectorAll('.square-div');
 
-newSquareDiv.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        event.target.style.backgroundColor = "black";
-    })
-})
+
+class ClickandHold {
+    /**
+     * 
+     * @param {EventTarget} target The HTML element to apply the event to
+     * @param {Function} callback The function to run once the target is clicked and held
+     */
+    constructor(target, callback) {
+        this.target = target;
+        this.callback = callback;
+        this.isHeld = false;
+        this.activeHoldTimeoutId = null;
+
+        ["mousedown", "touchstart"].forEach(type => {
+            this.target.addEventListener(type, this._onHoldStart.bind(this));
+        });
+
+        ["mouseup", "mouseleave", "mouseout", "touchend", "touchcancel"].forEach(type => {
+            this.target.addEventListener(type, this._onHoldEnd.bind(this));
+        });
+    }
+
+    _onHoldStart() {
+        this.isHeld = true;
+
+        this.activeHoldTimeoutId = setTimeout(() => {
+            if (this.isHeld) {
+                this.callback();
+            }
+        }, 1000)
+    }
+
+    _onHoldEnd() {
+        this.isHeld = false;
+        clearTimeout(this.activeHoldTimeoutId);
+    }
+}
