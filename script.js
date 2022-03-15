579dc6780fc51e86680f49d78e0948bdf969995e
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-03-15 19:54:44 +0100

    finally managed to get the hover effect working properly

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ for(let i = 0; i < 256; i++) {
 
 const newSquareDiv = document.querySelectorAll('.square-div');
 
+let count = 1;
+let click = () => count++;
+
+container.addEventListener('click', MouseOver);
+container.addEventListener('click', click);
+
+function MouseOver() {
+    newSquareDiv.forEach(div => {
+        div.addEventListener('mouseover', addBlack);
+        div.addEventListener('mouseover', addWhite);
+    });
+}
+
+function addBlack() {
+    this.style.backgroundColor = 'black';
+}
+
+function addWhite() {
+    if(count % 2) {
+        this.style.backgroundColor = 'white';
+    }
+}
+
+
+
+
+
+
+
+
+
+
