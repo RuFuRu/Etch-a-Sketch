fc8c3466ec3e0ad4cfd13fb77d440b8ce6d19915
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-03-15 20:45:30 +0100

    reset etch-a-sketch button - finished

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ for(let i = 0; i < 256; i++) {
     container.appendChild(squareDiv);
 }
 
-const newSquareDiv = document.querySelectorAll('.square-div');
+let newSquareDiv;
+newSquareDiv = document.querySelectorAll('.square-div');
 console.log(newSquareDiv)
 
 let count = 1;
@@ -45,6 +46,12 @@ button.addEventListener('click', clickAButton);
 
 function clickAButton() {
     let howManySquares = parseInt(prompt('How much squares would you like?'));
+    
+    if(howManySquares > 86) {
+        alert("That's too much squares, write another number");
+        return;
+    }
+
     container.removeAttribute('style', 'grid-template-columns');
     container.innerHTML = '';
     container.setAttribute('style',`grid-template-columns: repeat(${howManySquares}, 1fr);`)
@@ -52,8 +59,11 @@ function clickAButton() {
         squareDiv = document.createElement('div');
         squareDiv.setAttribute('class', 'square-div');
         container.appendChild(squareDiv);
+        newSquareDiv = document.querySelectorAll('.square-div');
     }
 }
+
+
 
 
 
